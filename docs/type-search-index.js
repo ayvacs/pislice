b92202c7bb6d3becfe7ca62d3a025ddd8375aa18
ayvacs <ayvacs@proton.me>
2024-02-28 16:30:26 +0000

    Deploying to gh-pages from @ ayvacs/pislice@56e4806de20dfcfc0eeefd1d70fa359cdc047c11 🚀

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"lol.pislice","l":"PiSlice"},{"p":"lol.pislice","l":"PiSliceException"}];updateSearchResults();
