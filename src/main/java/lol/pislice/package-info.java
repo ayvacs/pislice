e3899b74298540c9cc7e4c4c8074fc3def7c9ad6
dom <dompec07@gmail.com>
2024-02-29 10:58:20 -0500

    add package info

---
+++ src/main/java/lol/pislice/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A number derived from a substring of π. A <code>PiSlice</code> consists of a
+ * lower and upper bound from which a substring of π can be derived as a
+ * <code>long</code> or <code>int</code>.
+ */
+
+package lol.pislice;
